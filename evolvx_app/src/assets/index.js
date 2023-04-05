9f0d124b22b12f825c0a5bb325816021d95affe0
Code451F <451fsolo@gmail.com>
2023-04-05 20:20:15 +0530

    new changes

--- evolvx_app/src/assets/index.js
+++ evolvx_app/src/assets/index.js
@@ -24,6 +24,7 @@ import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
 import ex from "./logoEX.png";
+import robotarm from "./roboticarm.png";
 
 export {
   airbnb,
@@ -51,5 +52,6 @@ export {
   people01,
   people02,
   people03,
-  ex
+  ex,
+  robotarm
 };
