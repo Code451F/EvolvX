ad16608bb0ba0b6f205a5521c614c68be3bff008
chetanmanoj <chetanmanoj2001@gmail.com>
2023-04-05 20:20:55 +0530

    Initialized server

---
+++ evolvx_server/index.js
@@ -0,0 +1,13 @@
+// IMPORTS
+const express = require('express')
+
+
+//PORT
+const PORT = 6000;
+const app = express();
+app.use(express.json())
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+  });
