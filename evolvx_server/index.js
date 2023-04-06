87b6c7a92621021876a59b78b05984bbe2e00de9
chetanmanoj <chetanmanoj2001@gmail.com>
2023-04-06 13:40:07 +0530

    Implemented email API

--- evolvx_server/index.js
+++ evolvx_server/index.js
@@ -1,5 +1,7 @@
 // IMPORTS
 const express = require('express')
+const nodemailer = require('nodemailer')
+require('dotenv').config();
 
 
 //PORT
@@ -7,7 +9,46 @@ const PORT = 6000;
 const app = express();
 app.use(express.json())
 
-
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
   });
+
+//Receiving text
+app.post('/send-text', (req, res) => {
+    const text = req.body.text;
+    console.log(`Received text: ${text}`);
+})
+
+//nodemailer 
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
+
+  //SENDING MAIL
+  app.post("/send", function (req, res) {
+    let mailOptions = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: "Nodemailer API",
+      text: "Hi from your nodemailer API",
+    };
+   
+    transporter.sendMail(mailOptions, function (err, data) {
+      if (err) {
+        console.log("Error " + err);
+      } else {
+        console.log("Email sent successfully");
+        res.json({ status: "Email sent" });
+      }
+    });
+   });
+   
+
+   
+   
+
